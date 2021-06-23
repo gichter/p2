32b7a3a5407af372f7bfef41af151c91b739b103
Guillaume ICHTER <ichter.g@gmail.com>
2021-06-23 14:49:01 +0200

    removed pandas

--- webscraping.py
+++ webscraping.py
@@ -1,21 +1,17 @@
 import requests
 import re
 import csv
-import pandas as pd
 from bs4 import BeautifulSoup
 import os, shutil, pathlib
 import math
 
 book_total_number = 0
-
 main_folder = 'Book Data'
 
 if os.path.exists(main_folder):
     print("exists")
     shutil.rmtree(main_folder)
-
 os.mkdir(main_folder)
-
 
 response = requests.get("http://books.toscrape.com/")
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -27,16 +23,12 @@ for a in categories.select("li a"):
     if(category_name != "1/index.html"):
         pathlib.Path(main_folder + '/' + category_name).mkdir(parents=True, exist_ok=True) 
         print (category_name)
-
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-
         pages = soup.findAll('strong')
         number_of_pages = (math.ceil(int(pages[1].text)/20)) # Nombre de pages dans une catégorie
-
         header = ["product_page_url", "universal_product_code (upc)", "title", "price_including_tax", "price_excluding_tax", "number_available",
             "product_description", "category", "review_rating", "image_url"]
-
         with open(main_folder + '/' + category_name + '/' + category_name + '.csv', 'w', encoding='UTF8') as f:
             writer = csv.writer(f)
             writer.writerow(header)
@@ -48,7 +40,6 @@ for a in categories.select("li a"):
             soup = BeautifulSoup(response.content, 'html.parser')
             books = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
             number_of_pages -= 1
-
             for b in books:
                 for a in b.select("h3 a"): 
                     book_url = "http://books.toscrape.com/catalogue/" + a.get('href')[9:]
@@ -62,52 +53,16 @@ for a in categories.select("li a"):
                     rating = soup.findAll('p')[2] ### TODO
                     image_url = "http://books.toscrape.com/" + soup.findAll('img')[0]['src'][6:] ### concatenating base url and image relative url
                     rating = soup.find('p', {'class': 'star-rating'})['class'][1] ### Looking for the star-rating class, and extracting the second class attribute (star rating 1-5)
-                    
-                    
                     image_reference = (title.text + "_image.jpg")
-
-
                     
                     book = [ book_url, tds[0].text, title.text, tds[3].text[1:], tds[2].text[1:], number_available, description, category, rating, image_url, image_reference ]
                     with open(main_folder + '/' +category_name + '/' + category_name + '.csv', 'a', encoding='UTF8') as f:
                         writer = csv.writer(f)
                         writer.writerow(book)
                         print(title.text)
-                        book_total_number += 1
-                    
+                        book_total_number += 1                    
                     img_data = requests.get(image_url).content
                     with open(main_folder + '/' +category_name + '/' + image_reference, 'wb') as handler:
                         handler.write(img_data)
 
-
 print("Nombre total de livres récupérés :" + str(book_total_number))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" 
-    print("http://books.toscrape.com/" + a.get('href'))
-
-df = pd.DataFrame(list())
-df.to_csv('book.csv')
-
-with open('book.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
-""" 
